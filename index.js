b78c8eff8c03322de959d5fbdc79036fee972a78
donellgill <donell.gill@outlook.com>
2021-03-14 20:05:20 -0400

    added index.js

---
+++ index.js
@@ -0,0 +1,77 @@
+let number = 8;
+
+function add(){
+    return number += 6;
+}
+
+function subtract(){
+    return number -= 4;
+}
+
+function multiply(){
+    return number *6;
+}
+
+function divide(){
+    return number /= 12;
+}
+
+let a = 9;
+let b = 3;
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
+
+let digits = 85;
+
+function increment(digits){
+    digits++
+    return digits;
+}
+
+function decrement(digits){
+    digits--
+    return digits;
+}
+
+let x = parseInt("5", 10);
+
+function makeInt(x){
+    return x;
+}
+
+let y = parseInt("9");
+
+function makeInt(y){
+    return y;
+}
+
+let v = parseInt("5.323", 10);
+
+function makeInt(){
+    return v;
+}
+let z = parseInt("sjlefeifsf", 10);
+
+function makeInt(z){
+    return z;
+}
+
+let abc = parseFloat("7.563");
+
+function preserveDecimal(abc){
+    return abc;
+}
+
