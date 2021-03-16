f2529a4943bc6e563779eefa3a2c9564af6eede6
donellgill <donell.gill@outlook.com>
2021-03-15 23:37:46 -0400

    completed assignment

--- index.js
+++ index.js
@@ -75,3 +75,10 @@ function preserveDecimal(abc){
     return abc;
 }
 
+function makeInt(string){
+    return parseInt(string, 10);
+}
+
+function preserveDecimal(string){
+    return parseFloat(string);
+}
